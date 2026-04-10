22af301f564b06ed2e8ce76ab9712b78206b6cbf
bumble-beezz <chelseapollard05@gmail.com>
2026-04-09 21:19:47 -0400

    idk

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -14,6 +14,18 @@ function loadTable(users){
     }
 }
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 10000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 async function main(){
     const users = await getUserData();
     loadTable(users);
