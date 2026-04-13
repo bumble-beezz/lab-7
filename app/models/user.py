f804f378f03d50810dc66ffacea8e1b8af037f1d
bumble-beezz <chelseapollard05@gmail.com>
2026-04-13 15:54:16 -0400

    whhoopsie

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,12 @@
 from sqlmodel import Field, SQLModel, Relationship
 from typing import Optional, List
 from pydantic import EmailStr
-from app.models.todos import Todo
 
-
-class UserBase(SQLModel,):
+class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
     email: EmailStr = Field(index=True, unique=True)
     password: str
-    role:str = ""
+    role: str = ""
 
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
